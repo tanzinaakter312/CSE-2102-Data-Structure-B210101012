fcd8a5531c34b43e9dfcac89c39ad8d6cf422ac9
tanzinaakter312 <tanzinaislam016@gmail.com>
2025-01-05 21:22:54 +0600

    Add files via upload

---
+++ Stack/Front in linked list.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *front=0;
+struct node *rear=0;
+void enqueue(int x){
+    struct node *new;
+    new=(struct node*)malloc(sizeof(struct node));
+    new->data=x;
+    new->next=NULL;
+    if(front==0&&rear==0){
+        front=rear=new;
+    }
+    else{
+        rear->next=new;
+        rear=new;
+    }
+}
+void display(){
+    struct node *temp=front;
+    if(front==0&&rear==0){
+        printf("Empty");
+    }
+    else{
+
+    while(temp!=0){
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }
+}
+}
+void dequeue(){
+    struct node *temp=front;
+if(front==0&&rear==0){
+    printf("Queue is empty");
+}
+else{
+    printf("%d",front->data);
+    front=front->next;
+    free(temp);
+}
+}
+int main(){
+    enqueue(10);
+    enqueue(11);
+    enqueue(12);
+     enqueue(13);
+      enqueue(14);
+       enqueue(15);
+    display();
+    dequeue();
+
+}
+
+
