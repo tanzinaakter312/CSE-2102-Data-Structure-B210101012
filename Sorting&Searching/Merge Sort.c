6bd4826266cc055b570130db20e8d832004cf00f
tanzinaakter312 <tanzinaislam016@gmail.com>
2025-01-18 16:30:51 +0600

    Add files via upload

---
+++ Sorting&Searching/Merge Sort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+void merge(int arr[],int left,int mid,int right) {
+    int i,j,k;
+    int n1=mid-left+1;
+    int n2=right-mid;
+    int leftArr[n1],rightArr[n2];
+    for (i=0;i<n1;i++)
+        leftArr[i]=arr[left+i];
+    for(j=0;j<n2;j++)
+        rightArr[j]=arr[mid+1+j];
+    i=0;
+    j=0;
+    k=left;
+    while(i<n1&&j<n2) {
+        if(leftArr[i]<=rightArr[j]) {
+            arr[k]=leftArr[i];
+            i++;
+        }
+        else {
+            arr[k]=rightArr[j];
+            j++;
+        }
+        k++;
+    }
+    while (i<n1) {
+        arr[k]=leftArr[i];
+        i++;
+        k++;
+    }
+    while (j<n2) {
+        arr[k]=rightArr[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int arr[],int left,int right) {
+    if (left<right) {
+        int mid=left+(right-left)/2;
+        mergeSort(arr,left,mid);
+        mergeSort(arr,mid+1,right);
+        merge(arr,left,mid,right);
+    }
+}
+
+int main() {
+    int arr[]={10,20,40,30,80,50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("Array is:");
+    mergeSort(arr, 0, n - 1);
+    for (int i=0;i<n;i++)
+    printf("%d ",arr[i]);
+    return 0;
+}
